ef2525c36baae4c3eb7b7c02d25ba5116a08ae0e
Hoang <21hoangphan@gmail.com>
2022-09-23 23:45:18 -0700

    first version

---
+++ public/js/Animation.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var typed = new Typed('.typing', {
+        strings: ['The Future', 'Better Phillips Hue'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+})
--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/about', (req, res) => {
-    res.send("About Page");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/MainPage.html'));
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/LoginPage.html'));
+});
+
+app.get('/signin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/SigninPage.html'));
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to: http://localhost:${port}/`));
